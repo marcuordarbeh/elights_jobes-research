252ab759fd33886119e6465bf20a8b090301eeb4
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2025-03-21 17:45:47 +0100

    Update db.rs

--- backend/src/config/db.rs
+++ backend/src/config/db.rs
@@ -2,8 +2,8 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 pub async fn init_pool(database_url: &str) -> Pool<Postgres> {
     PgPoolOptions::new()
-        .max_connections(5)
+        .max_connections(10)
         .connect(database_url)
         .await
-        .expect("Failed to create pool.")
+        .expect("Failed to create PostgreSQL pool.")
 }
