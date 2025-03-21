3106e88cf9a8afcd0ad7a2ef699d756659bbdd48
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2025-03-21 17:47:18 +0100

    Create redis.rs

---
+++ backend/src/config/redis.rs
@@ -0,0 +1,11 @@
+use redis::aio::ConnectionManager;
+use redis::Client;
+use std::env;
+
+pub async fn init_redis() -> ConnectionManager {
+    let redis_url = env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1/".into());
+    let client = Client::open(redis_url).expect("Failed to create Redis client");
+    ConnectionManager::new(client)
+        .await
+        .expect("Failed to create Redis connection manager")
+}
