efd43f12d628dd3a6b2a210f8a3a143c6d688fd8
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2025-03-22 15:48:31 +0100

    Update AuthService.ts

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -3,31 +3,22 @@ import axios from 'axios';
 const API_URL = '/api/';
 
 class AuthService {
-    login(username: string, password: string) {
-        return axios
-            .post(API_URL + 'login', {
-                username,
-                password
-            })
-            .then(response => {
-                if (response.data.token) {
-                    localStorage.setItem('user', JSON.stringify(response.data));
-                }
-                return response.data;
-            });
-    }
+  login(username: string, password: string) {
+    return axios.post(API_URL + 'login', { username, password }).then(response => {
+      if (response.data.token) {
+        localStorage.setItem('user', JSON.stringify({ token: response.data.token }));
+      }
+      return response.data;
+    });
+  }
 
-    logout() {
-        localStorage.removeItem('user');
-    }
+  logout() {
+    localStorage.removeItem('user');
+  }
 
-    register(username: string, password: string, role: string) {
-        return axios.post(API_URL + 'register', {
-            username,
-            password,
-            role
-        });
-    }
+  register(username: string, password: string, role: string) {
+    return axios.post(API_URL + 'register', { username, password, role });
+  }
 }
 
 export default new AuthService();
