c46097c5a8a0b4378d8692c428e0ab49bd0069ba
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2025-03-22 15:47:13 +0100

    Update UserService.ts

--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -3,19 +3,18 @@ import axios from 'axios';
 const API_URL = '/api/';
 
 class UserService {
-    getUserBoard() {
-        return axios.get(API_URL + 'dashboard', { headers: this.authHeader() });
-    }
+  getUserBoard() {
+    return axios.get(API_URL + 'dashboard', { headers: this.authHeader() });
+  }
 
-    authHeader() {
-        const user = JSON.parse(localStorage.getItem('user')!);
-
-        if (user && user.token) {
-            return { Authorization: 'Bearer ' + user.token };
-        } else {
-            return {};
-        }
+  authHeader() {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    if (user && user.token) {
+      return { Authorization: 'Bearer ' + user.token };
+    } else {
+      return {};
     }
+  }
 }
 
 export default new UserService();
