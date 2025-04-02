a8a3a2b503bd7494bbaf558b137447bd49555164
EErstandardgatesR Standard Gates <erstandardgates@ERs-MacBook-Pro.local>
2025-04-02 05:11:53 +0100

    love more

--- backend/src/config/redis.rs
+++ backend/src/config/redis.rs
@@ -11,8 +11,7 @@ pub async fn init_redis() -> ConnectionManager {
     ConnectionManager::new(client)
         .await
         .expect("Failed to create Redis connection manager")
-
-        
+}       
 // pub async fn init_redis() -> ConnectionManager {
 //     let redis_url = env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1/".into());
 //     let client = Client::open(redis_url).expect("Failed to create Redis client");
